b848006d26bfb48e66dc10da99fc56f990719766
jsuttle2 <jsuttle2@gmail.com>
2023-05-16 11:05:58 -0400

    added more files and code to html

---
+++ client/public/src/components/UserProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../component styles/userProfile.css';
+
+
+export default function UserProfile() {
+  return (
+    <div className="container userProfile">
+      <h1>User Profile goes here</h1>
+    </div>
+  );
+}
