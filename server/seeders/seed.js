314a95d2a0cace49b489f744466e4b28612aa189
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-05-16 11:06:41 -0400

    Random profile seeding progress!

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,36 +1,49 @@
-const Profile = require('../models/User');
-const animals = require('random-animals-pictures');
-const fetch = require('node-fetch');
+const User = require("../models/User");
+const animals = require("random-animals-pictures");
+const fetch = require("node-fetch");
 
- // Generate random profile data
-const generateProfileData = async () => {
+// Generate random User data
+const generateUserData = async () => {
   // Generate quote
-  const quoteURL = 'https://animechan.vercel.app/api/random';
+  const quoteURL = "https://animechan.vercel.app/api/random";
   const response = await fetch(quoteURL);
   const data = await response.json();
   // Generate name
-  const nameURL = 'https://api.fungenerators.com/name/generate?category=pet';
+  const nameURL = "https://api.fungenerators.com/name/generate?category=pet";
   const response2 = await fetch(nameURL);
   const data2 = await response2.json();
   // Generate image
   const url = await animals.bird();
   return {
-    name: data2.names[0],
+    birdname: data2.names[0],
     img: url,
     quote: data.quote,
   };
 };
-
- // Seed the database with profiles
-const seedProfiles = async (numProfiles) => {
-  for (let i = 0; i < numProfiles; i++) {
-    const profileData = await generateProfileData();
-    const profile = new Profile(profileData);
-    await profile.save();
-  }
-  console.log(`Successfully seeded ${numProfiles} profiles`);;
+const generateEntryData = async (name) => {
+  const randomNum = Math.floor(Math.random() * 100);
+  const username = `${name}${randomNum}`;
+  const password = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const domain = 'example.com';
+  const email = `${username}@${domain}`;
+  return {
+    username: username,
+    password: password,
+    email: email,
+  };
 };
 
- // Seed profiles, insert how many
-seedProfiles(10);
 
+// Seed the database with Users
+const seedUsers = async (numUsers) => {
+  for (let i = 0; i < numUsers; i++) {
+    const userData = await generateUserData();
+    const entryData = await generateEntryData(userData.birdname);
+    const user = new User({...userData, ...entryData});
+    await user.save();
+  }
+  console.log(`Successfully seeded ${numUsers} users`);
+};
+
+// Seed users, insert how many
+seedUsers(10);
