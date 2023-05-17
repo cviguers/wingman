bdc4adafbcaa0ab786b80387ec2d21a542ff8a6b
jsuttle2 <jsuttle2@gmail.com>
2023-05-17 14:04:26 -0400

    added popup function for sign up and more styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,37 @@
-import React from 'react';
+import React from "react";
 
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // We import our Welcome component from our components folder so that we can eventually return it.
 
-import './index.css'; 
-import Welcome from './components/Welcome';
-import WelcomeForm from './components/WelcomeForm';
-import Dashboard  from './components/Dashboard';
-import UserProfile from './components/UserProfile';
-import BirdProfile from './components/BirdProfile';
+import "./index.css";
+import Welcome from "./pages/Welcome";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import UserProfile from "./pages/UserProfile";
+import BirdProfile from "./pages/BirdProfile";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -43,18 +44,19 @@ const client = new ApolloClient({
 
 // Inside our App component, we have a return method that contains all the JSX we want to render to the screen.
 // In this example, we have a parent `div` that references the Welcome component that we imported at the top.
+
  function App() {
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <Routes>
-        <Route exact path="/" component={Welcome} />
-        <Route path="/welcome-form" component={WelcomeForm} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/user-profile" component={UserProfile} />
-        <Route path="/bird-profile" component={BirdProfile} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Welcome />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/user-profile" element={<UserProfile />} />
+          <Route path="/bird-profile" element={<BirdProfile />} />
+        </Routes>
+      </Router>
     </ApolloProvider>
   );
 }
---
+++ client/src/components/Popup.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Signup from '../pages/Signup';
+
+const Popup = ({ onClose }) => {
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <button className="close-button" onClick={onClose}>
+          Close
+        </button>
+        <h2>Tell Us About Yourself</h2>
+        <Signup />
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
